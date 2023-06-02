3f56c3f5703fca9f24ac6bc6ffb39b0622df6aa7
Justin Hui <jhui@lcs.on.ca>
2023-06-02 09:09:37 -0400

    Deleted content view and added a home view and outfit view

---
+++ WeatherWear/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WeatherWear
+//
+//  Created by Justin Hui on 2/6/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ WeatherWear/Views/OutfitView.swift
@@ -0,0 +1,20 @@
+//
+//  OutfitView.swift
+//  WeatherWear
+//
+//  Created by Justin Hui on 2/6/2023.
+//
+
+import SwiftUI
+
+struct OutfitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OutfitView_Previews: PreviewProvider {
+    static var previews: some View {
+        OutfitView()
+    }
+}
