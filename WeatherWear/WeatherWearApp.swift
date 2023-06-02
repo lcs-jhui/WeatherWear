579c581f0df368ac922354f4af2c2e519ba14b69
Justin Hui <jhui@lcs.on.ca>
2023-06-02 09:04:53 -0400

    Initial Commit

---
+++ WeatherWear/WeatherWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWearApp.swift
+//  WeatherWear
+//
+//  Created by Justin Hui on 2/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
