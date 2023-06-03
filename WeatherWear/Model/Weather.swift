8fc64bcc17d6d45f247ee55c7713fc6bb96582c1
Justin Hui <jhui@lcs.on.ca>
2023-06-03 11:36:19 -0400

    Added a model to match the structure of the JSON object

---
+++ WeatherWear/Model/Weather.swift
@@ -0,0 +1,16 @@
+//
+//  Weather.swift
+//  WeatherWear
+//
+//  Created by Justin Hui on 3/6/2023.
+//
+
+import Foundation
+
+struct Weather {
+    let temperature: String
+    let wind: String
+    let description: String
+}
+
+let exampleWeather = Weather(temperature: "19 ℃", wind: "28 km/h", description: "Sunny")
