db72c2320c08a7d3c8cfd865f596685427770630
Justin Hui <jhui@lcs.on.ca>
2023-06-03 09:57:42 -0400

    Added the date, humidity and the precipitation to the outfit view

--- WeatherWear/Views/OutfitView.swift
+++ WeatherWear/Views/OutfitView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct OutfitView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            
+            VStack{
+                
+                //Show the date
+                Text("Monday, 29th May")
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(.infinity)
+                    .font(.title3)
+                
+                HStack{
+                    Text("Humidity: 44%")
+                        .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(.infinity)
+                    Text("Precipitation: 0mm")
+                        .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(.infinity)
+                }
+                
+            }
+            
+            .navigationTitle("Outfit")
+        }
     }
 }
 
