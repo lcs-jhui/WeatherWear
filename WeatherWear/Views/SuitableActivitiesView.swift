c5d59e5346f48c46beb655af0bff05d9a813c3a3
Justin Hui <jhui@lcs.on.ca>
2023-06-06 15:34:37 -0400

    Added the list of the activities to the List view helper

--- WeatherWear/Views/SuitableActivitiesView.swift
+++ WeatherWear/Views/SuitableActivitiesView.swift
@@ -5,16 +5,44 @@
 //  Created by Justin Hui on 6/6/2023.
 //
 
+import Blackbird
 import SwiftUI
 
 struct SuitableActivitiesView: View {
+    
+    //MARK: Stored Proeprties
+    
+    //Access the connection to the database
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+
+    //The list of activities for the weather
+    @BlackbirdLiveModels({ db in
+        try await Activity.read(from: db)
+    }) var activites
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        //A list of the activities
+        List{
+            
+            ForEach (activites.results) { currentActivity in
+                
+                Text(currentActivity.name)
+                    .font(.title3)
+                
+            }
+        }
+        .listStyle(.inset)
+        .border(.black)
+        .padding(.horizontal)
+        
     }
 }
 
 struct SuitableActivitiesView_Previews: PreviewProvider {
     static var previews: some View {
         SuitableActivitiesView()
+        //Make the database avaiable to this view in Xcode previews
+        .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
