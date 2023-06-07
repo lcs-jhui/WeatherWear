3bd6b9aac1f8514f072384d74a639ba31b6c82e0
Justin Hui <jhui@lcs.on.ca>
2023-06-07 10:30:14 -0400

    Added a blackbirdlivequery

--- WeatherWear/Views/SuitableActivitiesView.swift
+++ WeatherWear/Views/SuitableActivitiesView.swift
@@ -12,24 +12,26 @@ struct SuitableActivitiesView: View {
     
     //MARK: Stored Proeprties
     
-    //Access the connection to the database
+    //Needed to query database
     @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
-
-    //The list of activities for the weather
-    @BlackbirdLiveModels({ db in
-        try await Activity.read(from: db)
-    }) var activites
+    
+    //Weather to show
+    var temperatureForWeather: Weather
+    
+    //The list of activities
+    @BlackbirdLiveQuery(tableName: "Activity", { db in
+        try await db.query("SELECT name FROM Activity WHERE temp_low <= -10 AND temp_high >= -10")
+    }) var activities
     
     var body: some View {
-
+        
         //A list of the activities
         List{
             
-            ForEach (activites.results) { currentActivity in
+            ForEach (activities.results, id: \.self) { currentActivity in
                 
-                Text(currentActivity.name)
-                    .font(.title3)
-                
+                    Text(currentActivity.name)
+                        .font(.title3)
             }
         }
         .listStyle(.inset)
@@ -37,12 +39,15 @@ struct SuitableActivitiesView: View {
         .padding(.horizontal)
         
     }
+    
+    //MARK: Initializer
+    
 }
 
 struct SuitableActivitiesView_Previews: PreviewProvider {
     static var previews: some View {
-        SuitableActivitiesView()
+        SuitableActivitiesView(temperatureForWeather: exampleWeather)
         //Make the database avaiable to this view in Xcode previews
-        .environment(\.blackbirdDatabase, AppDatabase.instance)
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
